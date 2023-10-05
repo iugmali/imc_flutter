41197f2f4448e3bf88ef8567a3b4248c536d8cf3
Guilherme Pinto Lima de Almeida <dev@iugmali.com>
2023-10-05 15:13:15 -0300

    update hive initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:imc_flutter/models/imc.dart';
 import 'package:imc_flutter/my_app.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:hive/hive.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  var documentsDirectory = await getApplicationDocumentsDirectory();
-  Hive.init(documentsDirectory.path);
+  await Hive.initFlutter();
   Hive.registerAdapter(ImcAdapter());
   runApp(const MyApp());
 }
